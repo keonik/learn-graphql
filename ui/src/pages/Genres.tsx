5bd167e5ebbd7016a03baa6d5604546929eb3c71
John Fay <john.k.fay@gmail.com>
2020-04-23 16:08:40 -0400

    query genres and type generation

--- ui/src/pages/Genres.tsx
+++ ui/src/pages/Genres.tsx
@@ -1,7 +1,39 @@
 import React, { ReactElement } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import { GenresBookId } from '../typings/_graphql';
+
+const QUERY_GENRES = gql`
+    query genresBookId {
+        genres {
+            id
+            name
+            books {
+                id
+            }
+        }
+    }
+`;
+
+const UPDATE_GENRE = gql``;
 
 interface Props {}
 
 export default function Genres({}: Props): ReactElement {
-    return <div>Genres</div>;
+    const { data, loading, error } = useQuery<GenresBookId>(QUERY_GENRES);
+
+    return (
+        <div>
+            {loading && <h2>Loading genres....</h2>}
+            {error && <h2>Error finding genres...{error.message}</h2>}
+            <ol>
+                {data?.genres.map((genre: any) => (
+                    <li key={genre.id}>
+                        <p>Name: {genre.name}</p>
+                        <p>Book Count: {genre.books.length}</p>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
 }
---
+++ ui/src/typings/_graphql.ts
@@ -0,0 +1,36 @@
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: GenresBookId
+// ====================================================
+
+export interface GenresBookIdGenresBooks {
+  readonly id: string;
+}
+
+export interface GenresBookIdGenres {
+  readonly id: string;
+  readonly name: string;
+  readonly books: ReadonlyArray<GenresBookIdGenresBooks>;
+}
+
+export interface GenresBookId {
+  readonly genres: ReadonlyArray<GenresBookIdGenres>;
+}
+
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
