4cdf091ee0979bc6bf0bead579a225d7bda9ced8
John Fay <john.k.fay@gmail.com>
2020-04-24 00:14:44 -0400

    book mutations

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Routes, Route, Link, useMatch, useNavigate, useLocation } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import Genres from './pages/Genres';
 import Books from './pages/Books';
 import Genre from './pages/Genre';
 import Book from './pages/Book';
 import './App.css';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:9000/graphql',
+});
 
 function App() {
     const { pathname } = useLocation();
@@ -12,28 +18,30 @@ function App() {
     console.log(pathname === '/genres');
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <Link to="/" className={pathname === '/' ? 'Active' : ''}>
-                    Home
-                </Link>
-                <Link to="/genres" className={pathname === '/genres' ? 'Active' : ''}>
-                    Genres
-                </Link>
-                <Link to="/books" className={pathname === '/books' ? 'Active' : ''}>
-                    Books
-                </Link>
-            </header>
-            <section>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/genres" element={<Genres />} />
-                    <Route path="/genre:genreId" element={<Genre />} />
-                    <Route path="/books" element={<Books />} />
-                    <Route path="/book:bookId" element={<Book />} />
-                </Routes>
-            </section>
-        </div>
+        <ApolloProvider client={client}>
+            <div className="App">
+                <header className="App-header">
+                    <Link to="/" className={pathname === '/' ? 'Active' : ''}>
+                        Home
+                    </Link>
+                    <Link to="/genres" className={pathname === '/genres' ? 'Active' : ''}>
+                        Genres
+                    </Link>
+                    <Link to="/books" className={pathname === '/books' ? 'Active' : ''}>
+                        Books
+                    </Link>
+                </header>
+                <section>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/genres" element={<Genres />} />
+                        <Route path="/genre:genreId" element={<Genre />} />
+                        <Route path="/books" element={<Books />} />
+                        <Route path="/book:bookId" element={<Book />} />
+                    </Routes>
+                </section>
+            </div>
+        </ApolloProvider>
     );
 }
 
--- ui/src/pages/Books.tsx
+++ ui/src/pages/Books.tsx
@@ -1,7 +1,99 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { useNavigate } from 'react-router-dom';
+import { Drawer, Form, Input, Button } from 'antd';
+import { Books as BookQuery, CreateBook, CreateBookVariables } from '../typings/_graphql';
+import './Books.css';
+import 'antd/dist/antd.dark.css';
+
+const QUERY_BOOKS = gql`
+    query Books {
+        books {
+            id
+            title
+            author
+            description
+            published
+        }
+    }
+`;
+
+const CREATE_BOOK = gql`
+    mutation CreateBook($title: String!, $author: String!, $description: String) {
+        createBook(data: { title: $title, author: $author, description: $description }) {
+            id
+            title
+            author
+            description
+        }
+    }
+`;
 
 interface Props {}
 
 export default function Books({}: Props): ReactElement {
-    return <div>Books</div>;
+    const { data, loading, error } = useQuery<BookQuery>(QUERY_BOOKS);
+    const navigate = useNavigate();
+    const [open, setOpen] = useState<boolean>(false);
+
+    const [createBook, { loading: createLoading, error: createError }] = useMutation<CreateBook, CreateBookVariables>(
+        CREATE_BOOK,
+        {
+            refetchQueries: [{ query: QUERY_BOOKS }],
+        }
+    );
+
+    const onFinish = ({ author, title, description = '' }: any) => {
+        createBook({ variables: { author, title, description } }).then(() => {
+            setOpen(false);
+        });
+    };
+
+    return (
+        <section>
+            <Button type="primary" onClick={() => setOpen(true)}>
+                Create Book
+            </Button>
+            <div className="Books">
+                {loading && <h3>...Loading</h3>}
+                {error && <h3>Error: {error.message}</h3>}
+                {data?.books.map(({ author, description, id, published, title }) => (
+                    <span
+                        key={id}
+                        onClick={() => {
+                            navigate(`/book/${id}`);
+                        }}
+                        className="Book"
+                    >
+                        <img
+                            src="https://lh3.googleusercontent.com/proxy/gSWGlPo9iG6sFHMhFBmHQKbZzKnuM-JOlK7aKVMamTwhfICAFB2S2Nv24wch8dBcmB3_0wGfpOW4Ux6_unepoUSWOgOGGE4"
+                            width={100}
+                        />
+                        <p>Title: {title}</p>
+                        <p>Author: {author}</p>
+                        <p>Description: {description}</p>
+                    </span>
+                ))}
+            </div>
+            <Drawer visible={open} width={500} onClose={() => setOpen(false)}>
+                <h2>Create Book</h2>
+                <Form onFinish={onFinish}>
+                    <Form.Item name="title" label="Title">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="author" label="Author">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name="description" label="Description">
+                        <Input />
+                    </Form.Item>
+                    <Button type="primary" htmlType="submit" loading={createLoading}>
+                        Create
+                    </Button>
+                </Form>
+                {createError && <p>Error: {createError.message}</p>}
+            </Drawer>
+        </section>
+    );
 }
